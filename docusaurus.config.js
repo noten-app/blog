b3cef006cec85f67440a1042ffba2bd38c346b39
Konstantin Protzen <code@kondev.de>
2023-12-14 18:07:39 +0100

    🔧 Add social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -64,7 +64,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: 'https://assets.noten-app.de/images/banners/bannerAppLogoLandscape.png',
       navbar: {
         title: 'Noten-App',
         logo: {
